61a679489333982709aa400a7922c74af4a93ce4
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-08 20:16:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -105,7 +105,7 @@ end
 
   def movies_with_directors_set(source)
 
-binding.pry
+
 new_array = []
 
 index = 0
@@ -115,7 +115,7 @@ name = source[index][:name]
 movies = source[index][:movies]
 
 new_array << movies_with_director_key(name, movies)
-
+binding.pry
   index += 1
 
 end
